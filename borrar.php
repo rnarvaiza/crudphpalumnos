1ef8e51d78ac5e7365036a4daaa01461b093a9f3
rnarvaiza <rnm7687@gmail.com>
2021-04-30 13:04:56 +0200

    as

--- borrar.php
+++ borrar.php
@@ -12,7 +12,7 @@ try {
   $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
   $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
     
-  $id = $_GET['alumnoId'];
+  $id = $_GET['id'];
   $consultaSQL = "DELETE FROM alumnos WHERE id =" . $id;
 
   $sentencia = $conexion->prepare($consultaSQL);
