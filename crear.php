2b3be9b609b9cc9c30ff1474190e79bcd8be97bb
VZH <vzh@pop-os.localdomain>
2021-04-30 11:41:35 +0200

    1st commit

---
+++ crear.php
@@ -0,0 +1,97 @@
+<?php
+
+include 'funciones.php';
+
+csrf();
+if (isset($_POST['submit']) && !hash_equals($_SESSION['csrf'], $_POST['csrf'])) {
+  die();
+}
+
+if (isset($_POST['submit'])) {
+  $resultado = [
+    'error' => false,
+    'mensaje' => 'El alumno ' . escapar($_POST['nombre']) . ' ha sido agregado con éxito'
+  ];
+
+  $config = include 'config.php';
+
+  try {
+    $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
+    $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
+
+    $alumno = [
+      "nombre"   => $_POST['nombre'],
+      "apellido" => $_POST['apellido'],
+      "email"    => $_POST['email'],
+      "telefono" => $_POST['telefono'],
+      "fecha_nacimiento" => $_POST['fecha_nacimiento'],
+    ];
+
+    $consultaSQL = "INSERT INTO alumnos (nombre, apellido, email, telefono, fecha_nacimiento)";
+    $consultaSQL .= "values (:" . implode(", :", array_keys($alumno)) . ")";
+
+    $sentencia = $conexion->prepare($consultaSQL);
+    $sentencia->execute($alumno);
+
+  } catch(PDOException $error) {
+    $resultado['error'] = true;
+    $resultado['mensaje'] = $error->getMessage();
+  }
+}
+?>
+
+<?php include 'templates/header.php'; ?>
+
+<?php
+if (isset($resultado)) {
+  ?>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="alert alert-<?= $resultado['error'] ? 'danger' : 'success' ?>" role="alert">
+          <?= $resultado['mensaje'] ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+}
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2 class="mt-4">Crea un alumno</h2>
+      <hr>
+      <form method="post">
+        <div class="form-group">
+          <label for="nombre">Nombre</label>
+          <input type="text" name="nombre" id="nombre" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="apellido">Apellido</label>
+          <input type="text" name="apellido" id="apellido" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" class="form-control">
+        </div>
+          <div class="form-group">
+              <label for="telefono">Teléfono</label>
+              <input type="phone" name="telefono" id="telefono" class="form-control">
+          </div>
+        <div class="form-group">
+          <label for="fecha_nacimiento">Fecha de nacimiento</label>
+          <input type="date" name="fecha_nacimiento" id="fecha_nacimiento" class="form-control">
+        </div>
+        <div class="form-group">
+          <input name="csrf" type="hidden" value="<?php echo escapar($_SESSION['csrf']); ?>">
+          <input type="submit" name="submit" class="btn btn-primary" value="Enviar">
+          <a class="btn btn-primary" href="index.php">Regresar al inicio</a>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<?php include 'templates/footer.php'; ?>
