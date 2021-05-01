77b28efc45c0e6ca7045bb68a85d258769aff26f
rnarvaiza <rnm7687@gmail.com>
2021-05-01 12:14:47 +0200

    as

--- notas.php
+++ notas.php
@@ -52,7 +52,8 @@ if ($error) {
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <a href="crear.php"  class="btn btn-primary mt-4">Crear notas</a>
+                <a href="crear_nota.php"  class="btn btn-primary mt-4">Crear notas</a>
+                <a href="notas.php"  class="btn btn-primary mt-4">Volver</a>
                 <hr>
                 <form method="post" class="form-inline">
                     <div class="form-group mr-3">
